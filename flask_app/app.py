1479b3358230979f3fd269b625b1ac611dd49ad4
Denis <den.is.cher2002@gmail.com>
2024-04-20 15:31:10 +0300

    path changes and a bit of refactor

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,15 +3,13 @@ import os.path
 import numpy as np
 import torch
 import torch.nn as nn
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from PIL import Image
 from ultralytics import YOLO
-# from ml.models.segmentation.segmentation import UNet
 import segmentation_models_pytorch as smp
-from ml.models.segmentation.utils import CLASSES_IDS_TO_LABELS_DICT, CLASSES_IDS_TO_COLORS_DICT
+from ml.models.segmentation.utils import CLASSES_IDS_TO_COLORS_DICT
 from datetime import datetime
-import matplotlib.patches as mpatches
 
 app = Flask(__name__)
 
@@ -25,28 +23,23 @@ def model():
     data = request.get_json()
     width, height = data['width'], data['height']
     img_data = np.array(list(data['image'].values())).astype('uint8')
-    R = img_data[0::4].reshape(height, width)
-    G = img_data[1::4].reshape(height, width)
-    B = img_data[2::4].reshape(height, width)
+    R,G,B = img_data[0::4].reshape(height, width), img_data[1::4].reshape(height, width), img_data[2::4].reshape(height, width)
 
     image = np.transpose(np.stack([R, G, B], axis=0), (1, 2, 0))
     pil_image = Image.fromarray(image)
 
-    # YOLO
     yolo = YOLO('model_weights/yolo8_weights.pt')
     pil_image.resize((640, 640)).save('detection_tmp.png')
     result = yolo(['detection_tmp.png'])
     img_detection = result[0].plot(font_size=12, pil=True, labels=True, line_width=2)
 
-    gallery_dir = 'images_gallery'
+    gallery_dir = '../app/images_gallery'
     if not os.path.exists(gallery_dir):
         os.mkdir(gallery_dir)
 
     path_detection = f'{gallery_dir}/detection{timestamp}.png'
     Image.fromarray(img_detection).resize((1024, 1024)).save(path_detection)
 
-
-    # U-Net
     unet = smp.Unet(
         encoder_name='resnet50',
         encoder_weights='imagenet',
@@ -62,13 +55,10 @@ def model():
     preds = torch.argmax(softmax(outputs), dim=1)[0].numpy()
 
     new_preds = np.ndarray(shape=(preds.shape[0], preds.shape[1], 3), dtype='uint8')
-    # new_preds_unique = np.unique(preds.ravel())
+
     for j in range(0, preds.shape[0]):
         for k in range(0, preds.shape[1]):
             new_preds[j][k] = CLASSES_IDS_TO_COLORS_DICT[preds[j][k]]
-
-    # patches_pred = [mpatches.Patch(color=list(map(lambda x: x / 255, CLASSES_IDS_TO_COLORS_DICT[value])),
-    #                                label=f"{CLASSES_IDS_TO_LABELS_DICT[value]}") for value in new_preds_unique]
 
     init_image = Image.fromarray(image_segmentation).convert("RGBA")
     segm_result = Image.fromarray(new_preds).convert("RGBA")
@@ -83,10 +73,11 @@ def model():
     path_mask = f'{gallery_dir}/mask{timestamp}.png'
     segm_result.resize((1024, 1024)).save(path_mask)
 
+    skip_path_elems = 7
     return jsonify({
-        'path_segmentation': '../../' + path_segmentation
-        , 'path_detection': '../../' + path_detection
-        , 'path_mask_only': '../../' + path_mask
+        'path_segmentation': path_segmentation[skip_path_elems:]
+        , 'path_detection':  path_detection[skip_path_elems:]
+        , 'path_mask_only': path_mask[skip_path_elems:]
     })
 
 
